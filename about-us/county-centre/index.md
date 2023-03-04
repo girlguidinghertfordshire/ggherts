1d104c8b52998f958730a380fc242cd48b54e379
reallyandrew <andrew@andrewsettle.com>
2023-03-04 17:11:03 +0000

    Updates county centre page to new branding

--- about-us/county-centre/index.md
+++ about-us/county-centre/index.md
@@ -1,19 +1,14 @@
 ---
-layout: page
+layout: page-v5
 title: County Centre
 date: 2016-03-04T12:50:17.000Z
 toc: about-us
 location_map: true
 advertId: get-involved
 showMap: true
+validation: countycentreform
 description: The Hertfordshire Guide Centre is located in the village of Cottered in North
   Herts, and is suitable for meetings, trainings, sleepovers and camps.
-author:
-  login: GGH-Admin1
-  email: share@girlguidinghertfordshire.org.uk
-  display_name: GGH-Admin1
-  first_name: ""
-  last_name: ""
 permalink: /about-us/county-centre/
 lastmod: 2022-09-07T20:48:28.022Z
 ---
@@ -31,9 +26,13 @@ Please visit the [Hertfordshire Guide Centre's dedicated website](http://www.her
 - View current hire fees
 - Make booking requests.
 
-Contact the HGC Bookings Secretary via <HGCBookings@girlguidinghertfordshire.org.uk> with any queries, or submit them via the Guide Centre website's Contact Us page.
+Contact the HGC Bookings Secretary via <HGCBookings@girlguidinghertfordshire.org.uk> with any queries, or submit them below.
 
-## <img loading="lazy" class="alignleft wp-image-3945" style="margin-right: 10px;" src="/wp-content/uploads/2020/12/200-Club-Logo-150x150.jpg" alt="" width="115" height="115" srcset="/wp-content/uploads/2020/12/200-Club-Logo-150x150.jpg 150w, /wp-content/uploads/2020/12/200-Club-Logo.jpg 184w" sizes="(max-width: 115px) 100vw, 115px" />Hertfordshire Guide Centre 200 Club
+## Contact form
+
+{% include countycentreform.html %}
+
+## ![200 club logo](/wp-content/uploads/2020/12/200-Club-Logo-150x150.jpg){:loading="lazy"}{:.float-start.me-2}Hertfordshire Guide Centre 200 Club
 
 For Girlguiding Hertfordshire members or non-Guiding family members and friends.  
 
@@ -43,9 +42,11 @@ A one off payment of £15* gives you the opportunity to join the monthly prize d
 
 If you would like to join the 200 Club please complete the online registration form and download and read the terms and conditions via the buttons below.
 
-[Online Application Form <i class="fa fa-external-link"></i>](https://forms.office.com/Pages/ResponsePage.aspx?id=3yob_CzTykeMNWNnWM6OwYCE4GYtXJ9Ogtjv7oAM_iJUREpYRDhXREFSRVJWU0RPREhSWlhLUFNZUC4u){.btn.btn-primary}{:target="_blank"}{:rel="noopener"} [Terms and Conditions <i class="fa fa-download"></i>](/assets/docs/2022/2023-200-club-tscs.pdf){:.btn.btn-primary}{:target="_blank"}{:rel="noopener"}
+[Online Application Form](https://forms.office.com/Pages/ResponsePage.aspx?id=3yob_CzTykeMNWNnWM6OwYCE4GYtXJ9Ogtjv7oAM_iJUREpYRDhXREFSRVJWU0RPREhSWlhLUFNZUC4u){:.btn.btn-primary.external-link}{:target="_blank"}{:rel="noopener"} [Terms and Conditions](/assets/docs/2022/2023-200-club-tscs.pdf){:.btn.btn-primary.download-link}{:target="_blank"}{:rel="noopener"}
 
-To view the names of recent winners please visit the 200 Club webpage on the Hertfordshire Guide Centre website. [200 Club Results <i class="fa fa-external-link"></i>](https://www.hertsguidecentre.co.uk/200-club.html){:target="_blank"}{:.external-link}{:rel="noreferrer noopener"}
+To view the names of recent winners please visit the 200 Club webpage on the Hertfordshire Guide Centre website. [200 Club Results](https://www.hertsguidecentre.co.uk/200-club.html){:target="_blank"}{:.external-link}{:rel="noreferrer noopener"}
 
 {:.small}
 \* Those persons joining throughout the year may pay a pro rata payment for the remaining part of the year.
+
+{% include countycentre.html %}
---
+++ assets/js/countycentreform.js
@@ -0,0 +1,85 @@
+ (function($, url, key){
+    var errorGroup="has-error";
+    $("#hgcForm").validate({
+        highlight: function(element, errorClass, validClass) {
+            $(element).addClass(errorClass).removeClass(validClass);
+            $(element.form).find("label[for=" + element.id + "]").closest(".form-group")
+            .addClass(errorGroup);
+        },
+        unhighlight: function(element, errorClass, validClass) {
+            $(element).removeClass(errorClass).addClass(validClass);
+            $(element.form).find("label[for=" + element.id + "]").closest(".form-group")
+            .removeClass(errorGroup);
+        },
+        showErrors: function(){
+            $(".error-summary").remove();
+            if (this.numberOfInvalids()>0){
+               $("form .row").prepend(`<div class="col-sm-12 error-summary"><div class="alert alert-danger"><i class="fa fa-warning"></i> There ${(this.numberOfInvalids()===1?"is":"are")} ${ this.numberOfInvalids()} fields which need your attention, see details below.</div></div>`); 
+            }            
+            this.defaultShowErrors();
+        },
+        errorClass: "help-block"
+    });
+    function updateForm(msg, canHideForm, isError){
+        if (canHideForm){
+            $("form").hide();
+        }
+        $("form").before(`<div class="alert alert-${(isError?"danger":"success")}">${msg}</div>`);
+    }
+    $(window).on("recaptchaSubmit", function(e){   
+        console.log("event received...");
+        $(".submit-btn").prop("disabled",true);
+        $(".error-summary").remove();
+        if (!$("#hgcForm").valid()){
+            return;
+        }
+        var data= {
+            name: $("#name").val(),
+            email: $("#email_address").val(),
+            phone: $("#phone_number").val(),
+            message: $("#enquiry").val(),
+            specialMessage: $("#enquirydetail").val(),
+            recaptchaToken: e.detail.token
+        };
+        console.log(data,url,key);
+        
+        $.post(`${url}contactus?apikey=${key}`, JSON.stringify(data))
+            .done(function(result){
+                updateForm(result.statusMessage,true,false);
+            })
+            .fail(function(result){
+                console.log(result);
+                if (Object.prototype.hasOwnProperty.call(result, "status")) {
+                    switch (result.status) {
+                        case 400:
+                            if (Object.prototype.hasOwnProperty.call(result, "responseJSON") && Object.prototype.hasOwnProperty.call(result.responseJSON, "hasValidationError")) {
+                                updateForm(`An error occurred when processing your form: <ul><li>${result.responseJSON.statusMessage}</li></ul>`, false, true);
+                                break;
+                            }
+                        // eslint-disable-next-line no-fallthrough
+                        case 401:
+                        case 404:
+                            updateForm("Sorry, an error is preventing us from sending your form. Please contact us by email. We apologise for any inconvenience caused.", true, true);
+                            break;
+                        default:
+                            alert(`${result.status}: ${result.statusText}`);
+                    }
+                    if (Object.prototype.hasOwnProperty.call(result, "responseJSON") && Object.prototype.hasOwnProperty.call(result.responseJSON, "statusMessage")) {
+                        console.log(`${result.status}: ${result.statusText} - ${result.responseJSON.statusMessage}`);
+                    } else {
+                        console.log(`${result.status}: ${result.statusText}`);
+                    }
+                }
+            });   
+    })
+ // eslint-disable-next-line no-undef
+ })(jQuery, baseUrl, apikey);
+    
+    
+// eslint-disable-next-line no-unused-vars
+function onSubmit(token){    
+    console.log("recaptcha complete");
+    const event=new CustomEvent("recaptchaSubmit",{bubbles:true, detail:{token:token}});
+    console.log(event, this);
+    this.dispatchEvent(event); 
+}
