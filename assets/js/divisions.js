2ae1e9ff733bc4c535c5933d269aae122e3dabd2
reallyandrew <andrew@andrewsettle.com>
2023-05-29 08:26:30 +0100

    Fixes object instantiation in division map usage

--- assets/js/divisions.js
+++ assets/js/divisions.js
@@ -1,12 +1,12 @@
-
+// eslint-disable-next-line no-unused-vars
 class DivisionMap {
     google;
     map;
     kmlSrc = "https://www.girlguidinghertfordshire.org.uk/assets/js/divisions/maps_regions.kml?v=1";
     constructor() {
-        console.log("Constructing...");
         this.google = window.google || {};
-        console.log("constructing...", this.google);
+    }
+    render(){
         this.map = new this.google.maps.Map(document.getElementById('map'), {
             center: new this.google.maps.LatLng(51.778877, -0.223078),
             zoom: 10,
@@ -19,7 +19,6 @@ class DivisionMap {
         });
         console.log(kmlLayer);
         kmlLayer.addListener('click', function(event) {
-          var content = event.featureData.infoWindowHtml;
           console.log(event);
           const divisions = document.querySelectorAll(".division");
           divisions.forEach(function(division){
