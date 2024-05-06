d5e088cc72b3212acef9e4838cb2bffdf758677b
Andrew Settle <andrew.settle@synetec.co.uk>
2024-05-07 00:08:59 +0100

    Updates badge orders form

--- assets/js/awards.js
+++ assets/js/awards.js
@@ -1,7 +1,9 @@
 class Order {
     email;
     name;
-    girls = [];
+    unit;
+    district;
+    shop;
     #items = JSON.stringify([
         { "award": "bronze", "type": "certificate", "description": "Bronze Award - Certificate", "price": 0.7, "quantity": 0 },
         { "award": "bronze", "type": "woven", "description": "Bronze Award - Woven Badge", "price": 1.1, "quantity": 0 },
@@ -16,58 +18,8 @@ class Order {
     orderItems;
     deliveryOption = "Posted1";
     deliveryAddress;
+    message;
     orderTotal = 0;
-
-    girlRow() {
-        const counter = this.girls.length - 1;
-        return `<div class="row mb-3">
-        <div class="col-1 col-lg-auto align-self-end">${counter + 1}.</div>
-        <div class="col-7 col-md-4 col-lg">
-            <label for="girlsName${counter}" class="form-label">Name:</label>
-            <input type="text" class="form-control" minlength="3" maxlength="50" id="girlsName${counter}"
-                name="girls_name_${counter}" required />
-            <div class="invalid-feedback">Girls name is required</div>
-        </div>
-        <div class="col-4 col-md-2">
-            <label for="girlsNumber${counter}" class="form-label">Memb<span class="d-none d-xxl-inline">ership</span> number:</label>
-            <input type="tel" class="form-control" pattern="[0-9]{5,15}" minlength="5" maxlength="10" id="girlsNumber${counter}"
-                name="girls_number_${counter}" required />
-            <div class="invalid-feedback">Membership number is required</div>
-        </div>
-        <div class="col-1 d-md-none"></div>
-        <div class="col-11 col-md-5 col-lg">
-            <label for="girlsUnit${counter}" class="form-label">Unit:</label>
-            <input type="text" class="form-control" minlength="3" id="girlsUnit${counter}" name="girls_number_${counter}" required />
-            <div class="invalid-feedback">Unit name is required</div>
-        </div>
-        <div class="col-1 d-lg-none"></div>
-        <div class="col-6 col-md-3 col-lg">
-            <label for="girlsSection${counter}" class="form-label">Section:</label>
-            <select id="girlsSection${counter}" class="form-select js-section">
-                <option value="rainbows">Rainbows</option>
-                <option value="brownies">Brownies</option>
-                <option value="guides">Guides</option>
-                <option value="rangers">Rangers</option>
-            </select>
-        </div>
-        <div class="col-5 col-md-3 col-lg">
-            <label for="girlsAward${counter}" class="form-label">Award:</label>
-            <select id="girlsAward${counter}" class="form-select js-award">
-                <option value="bronze">Bronze</option>
-                <option value="silver">Silver</option>
-                <option value="gold">Gold</option>
-            </select>
-        </div>       
-        <div class="col-1 d-md-none"></div>
-        <div class="col-6 col-md-3 col-lg-auto mt-3 mt-lg-0">
-            <div class="form-check">
-                <label for="girlsComplete${counter}" class="form-check-label">Award completed?</label>
-                <input type="checkbox" class="form-check-input mt-lg-2" id="girlsComplete${counter}" value="yes" />
-            </div>
-        </div>
-        <div class="col-auto align-self-end d-none"><button type="button" class="btn btn-sm btn-outline-dark js-delete" id="girlsDelete${counter}" title="Remove recipient"><i class="fa fa-trash-o" aria-hidden="true"></i><span class="d-lg-none"> Remove</span</button>
-    </div>`;
-    }
 
     orderDetails(sections) {
         let orderTotal = 0;
@@ -78,13 +30,8 @@ class Order {
         Object.keys(sections).forEach(function (section) {
             const awards = sections[section];
             const items = orderItems[section];
-            if (awards["bronze"] > 0 || awards["silver"] > 0 || awards["gold"] > 0) {
-                order += sectionRow(section);
-            }
+                order += sectionRow(section);            
             for (const item of items) {
-                if (awards[item.award] == 0) {
-                    continue;
-                }
                 order += itemRow(item.description, `${section}.${item.award}.${item.type}`, item.price, item.quantity);
                 orderTotal += item.price * item.quantity;
             }
@@ -112,17 +59,6 @@ class Order {
         localStorage.setItem("awards", JSON.stringify(this));
     }
 
-    get girlCount() {
-        return this.girls.length;
-    }
-    get hasIncompleteAwards() {
-        for (const girl of this.girls) {
-            if (!girl.isComplete) {
-                return true;
-            }
-        }
-        return false;
-    }
     get deliveryMethod() { return this.deliveryOption; }
     set deliveryMethod(value) {
         switch (value) {
@@ -143,19 +79,17 @@ class Order {
         this.deliveryAddress = value;
         this.save();
     }
-    addGirl() {
-        this.girls.push(new Girl());
-    }
-    updateCustomer(name, email) {
-        this.name = name;
-        this.email = email;
+    get Message() {return this.message;}
+    set Message(value){
+        this.message=value;
         this.save();
     }
-    updateGirls(id, value) {
-        id = id.replace("girls", "");
-        const row = id.replace(/\D+/,"");
-        const field = id.replace(row, "");
-        this.girls[row][field] = value;
+    updateCustomer(name, email, unit, district, shop) {
+        this.name = name;
+        this.email = email;
+        this.unit = unit;
+        this.district=district;
+        this.shop=shop;
         this.save();
     }
     resetItems(awards) {
@@ -183,7 +117,6 @@ class Order {
         this.save();
     }
     constructor() {
-        this.girls.push(new Girl());
         this.orderItems = {
             "rainbows": JSON.parse(this.#items),
             "brownies": JSON.parse(this.#items),
@@ -194,69 +127,6 @@ class Order {
     }
 }
 
-class Girl {
-    name = "";
-    membershipNumber = 0;
-    isComplete = false;
-    awardLevel = "Bronze"
-    unit = "";
-    section = "rainbows";
-
-    /**
-     * @param {string} value
-     */
-    set Name(value) {
-        this.name = value;
-    }
-    /**
-     * @param {number} value
-     */
-    set Number(value) {
-        this.membershipNumber = value;
-    }
-    /**
-     * @param {string} value
-     */
-    set Unit(value) {
-        this.unit = value;
-    }
-    /**
-     * @param {string} value
-     */
-    set Award(value) {
-        this.awardLevel = value;
-    }
-    /**
-     * @param {boolean} value
-     */
-    set Complete(value) {
-        this.isComplete = value;
-    }    
-    /**
-     * @param {string} value
-     */
-    set Section(value){
-        this.section = value;
-    }
-    constructor(name, membershipNumber, isComplete, awardLevel, unit, section) {
-        if (isComplete == null) {
-            isComplete = false;
-        }
-        if (awardLevel == null) {
-            awardLevel = "Bronze";
-        }
-        if (section == null){
-            section="Rainbows";
-        }
-        this.name = name;
-        this.membershipNumber = membershipNumber;
-        this.isComplete = isComplete;
-        this.awardLevel = awardLevel;
-        this.unit = unit;
-        this.section=section;
-    }
-}
-
 const order = new Order();
 
 (function ($) {
@@ -264,17 +134,11 @@ const order = new Order();
 
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     const forms = document.querySelectorAll('.needs-validation')
-    const $recipients = $("#awardRecipients");
-    $recipients.append(order.girlRow());
     $("#customer").on("change", function () {
-        order.updateCustomer($("#order_name").val(), $("#order_email").val());
+        order.updateCustomer($("#order_name").val(), $("#order_email").val(), $("#order_unit").val(), $("#order_district").val(), $("#order_shop").val());
     });
     function isValid(fieldName) {
         const field = document.getElementById(fieldName);
-        if (fieldName.includes("girlsNumber")){
-            let number=field.value.replace(/[\D]+/,"");
-            field.value=number;
-        }
         return field.checkValidity();
     }
     function girlsAwards() {
@@ -305,35 +169,10 @@ const order = new Order();
             $("#deliveryDetails .form-check").eq(0).find("input").prop("checked",true);
         }
     }
-    $recipients.on("change", function (event) {
-        const value = event.target.id.indexOf("Complete") > -1 ? $(event.target).prop("checked") : $(event.target).val();
-        order.updateGirls(event.target.id, value);
-        const lastRow = order.girlCount - 1;
-        $("#incompleteNote").addClass("d-none");
-        $("#goldAward").addClass("d-none");
-        if (isValid(`girlsName${lastRow}`) && isValid(`girlsNumber${lastRow}`) && isValid(`girlsUnit${lastRow}`)) {
-            $("#addGirl").prop("disabled", false);
-            $("#sendOrder").prop("disabled", false);
             const awards = girlsAwards();
-            if (event.target.id.indexOf("Award") > -1) {
-                order.resetItems(awards);
-            }
-            $(".js-no-girls").hide();
-            $(".js-has-girls").removeClass("d-none");
-            Object.keys(awards).forEach(function (key) {
-                const section = awards[key];
-                if (section.gold > 0) {
-                    $("#goldAward").removeClass("d-none");
-                }
-            })
-            if (order.hasIncompleteAwards) {
-                $("#incompleteNote").removeClass("d-none");
-            }
-            $("#orderDetails table").removeClass("d-none");
             order.updateItems(awards);
             updateOrders(awards);
-        }
-    });
+    
     const $orderDetails = $("#orderDetails");
     $orderDetails.on("change", function (event) {
         order.updateQuantity(event.target.id, $(event.target).val());
@@ -364,12 +203,10 @@ const order = new Order();
     $("#postalAddress").on("change", function () {
         order.Address = $(this).val();
     });
-    $("#addGirl").on("click", function () {
-        order.addGirl();
-        $recipients.append(order.girlRow());
-        $("#addGirl").prop("disabled", true);
-        $("#sendOrder").prop("disabled", true);
+    $("#message").on("change", function() {
+        order.Message = $(this).val();
     });
+    
     // Loop over them and prevent submission
     Array.prototype.slice.call(forms)
         .forEach(function (form) {
@@ -386,7 +223,6 @@ const order = new Order();
 
 function validateForm(form) {
     $(".error-summary").remove();
-    console.log(form);
     if (!form.checkValidity()) {
         console.log("form invalid");
         let errors = $(".form-control:invalid").length;
@@ -412,10 +248,13 @@ function onSubmit(token) { // function is called by Recaptcha callback.
     const data = {
         name: order.name,
         email: order.email,
-        girls: order.girls,
+        unit: order.unit,
+        district: order.district,
+        shop: order.shop,
         orderItems: order.orderItems,
         deliveryOption: order.deliveryOption,
         deliveryAddress: order.deliveryAddress,
+        message: order.message,
         specialMessage: $("#enquirydetail").val(),
         recaptchaToken: token
     };
